ef2014c48ac3be75be4f94371b35a075cb1ce674
Peter Chapin <spicacality@kelseymountain.org>
2024-01-06 10:42:44 -0500

    Polished the basic array summation example in Julia.

--- Julia/Sum/sum.jl
+++ Julia/Sum/sum.jl
@@ -1,5 +1,14 @@
+# This is a simple example of how to use Julia's multi-threading capabilities to add numbers in
+# parallel. This example is a lightly modified version of code taken from the Julia
+# documentation: https://docs.julialang.org/en/v1/manual/multi-threading/#The-@threads-Macro
+
 using BenchmarkTools
 
+"""
+    sum_serial(a)
+
+Compute the sum of the elements of an array `a` serially.
+"""
 function sum_serial(a)
     # The obvious serial implementation
     s = 0
@@ -9,12 +18,12 @@ function sum_serial(a)
     return s
 end
 
-a = ones(100_000_000)
-println("Running serial sum...")
-println("\t", sum_serial(a))
-println("Benchmarking serial sum...")
-@btime sum_serial(a)
 
+"""
+    sum_multithread(a)
+
+Compute the sum of the elements of an array `a` using multiple threads.
+"""
 function sum_multithread(a)
     # Split problem into subproblems and map the subproblems to threads (mapping step)
     chunks = Iterators.partition(a, length(a) ÷ Threads.nthreads())
@@ -27,7 +36,15 @@ function sum_multithread(a)
     return sum_serial(chunk_sums)
 end
 
-println("Running multi-threaded sum...")
+# Testing
+a = ones(100_000_000)
+
+println("\nRunning serial sum...")
+println("\t", sum_serial(a))
+println("Benchmarking serial sum...")
+@btime sum_serial(a)
+
+println("\nRunning multi-threaded sum...")
 println("\t", sum_multithread(a))
 println("Benchmarking multi-threaded sum...")
 @btime sum_multithread(a)
