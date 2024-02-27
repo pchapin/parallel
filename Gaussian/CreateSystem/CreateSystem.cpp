f08c279880f1dcc8ebf2eac53a25e848bd38a8c5
Peter Chapin <spicacality@kelseymountain.org>
2024-02-27 09:38:58 -0500

    Updated CreateSystem and added an Eclipse/CDT project for it.

--- Gaussian/CreateSystem/CreateSystem.cpp
+++ Gaussian/CreateSystem/CreateSystem.cpp
@@ -1,14 +1,14 @@
 /*!
- *  \file   main.cpp
+ *  \file   CreateSystem.cpp
  *  \brief  Main program of the linear equations creator.
- *  \author (C) Copyright 2011 by Peter C. Chapin <pchapin@vtc.edu>
+ *  \author (C) Copyright 2024 by Peter Chapin <peter.chapin@vermontstate.edu>
  *
  * This program creates a random system of linear equations in a format that is acceptable to
- * the LinearEquations program.
+ * the Gaussian Elimination programs.
  *
  * Build this program using the command:
  *
- *    $ g++ -o CreateSystem -Wall CreateSystem.cpp
+ *    $ g++ -std=c++17 -o CreateSystem -Wall CreateSystem.cpp
  *
  * Run the program to create a system of a certain size (say 100x100) using the command:
  *
@@ -20,12 +20,10 @@
 #include <iomanip>
 #include <iostream>
 
-using namespace std;
-
 //! Generate a random double in the range -1.0 < x < 1.0
 double generate_value( )
 {
-    double value = static_cast<double>( rand( ) ) / RAND_MAX;
+    double value = static_cast<double>( std::rand( ) ) / RAND_MAX;
     return (2.0 * value) - 1.0;
 }
 
@@ -36,31 +34,32 @@ int main( int argc, char **argv )
 
     // Check command line validity.
     if( argc != 2 ) {
-        cerr << "Usage: " << argv[0] << " size\n";
+        std::cerr << "Usage: " << argv[0] << " size" << std::endl;
         return EXIT_FAILURE;
     }
 
     // Convert size argument to an integer.
-    size = atoi( argv[1] );
+    size = std::atoi( argv[1] );
 
     // Check the sanity of the size argument.
+    // TODO: Check against a reasonable upper bound also?
     if( size <= 0 ) {
-        cerr << "Invalid system size specified: " << size << "\n";
+        std::cerr << "Invalid system size specified: " << size << std::endl;
         return EXIT_FAILURE;
     }
 
-    cout << size << "\n";
+    std::cout << size << std::endl;
 
     // For each equation...
     for( int i = 1; i <= size; ++i ) {
 
         // Output a coefficient for each unknown.
         for( int j = 1; j <= size; ++j ) {
-            cout << setw( 18 ) << setprecision( 15 ) << fixed << generate_value( ) << "\n";
+            std::cout << std::setw( 18 ) << std::setprecision( 15 ) << std::fixed << generate_value( ) << std::endl;
         }
 
         // Output the driving vector value for this equation.
-        cout << setw( 18 ) << setprecision( 15 ) << fixed << generate_value( ) << "\n";
+        std::cout << std::setw( 18 ) << std::setprecision( 15 ) << std::fixed << generate_value( ) << std::endl;
     }
     return EXIT_SUCCESS;
 }
